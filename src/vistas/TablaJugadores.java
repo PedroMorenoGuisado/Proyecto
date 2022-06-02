01432a24a475570c1dd6a97d0ca7e451670cd158
PedroMorenoGuisado <pedro.moreno@adaits.es>
2022-06-02 18:21:18 +0200

    vista TablaJugadores

--- src/vistas/TablaJugadores.java
+++ src/vistas/TablaJugadores.java
@@ -6,10 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class TablaJugadores extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 
 	public TablaJugadores() {
@@ -20,6 +27,46 @@ public class TablaJugadores extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(142, 217, 89, 23);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(78, 43, 46, 14);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("victorias");
+		lblNewLabel_1.setBounds(78, 68, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("empates");
+		lblNewLabel_2.setBounds(78, 93, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("derrotas");
+		lblNewLabel_3.setBounds(78, 116, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		textField = new JTextField();
+		textField.setBounds(142, 40, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Total");
+		lblNewLabel_4.setBounds(78, 141, 46, 14);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Nivel");
+		lblNewLabel_5.setBounds(78, 166, 46, 14);
+		panel.add(lblNewLabel_5);
 	}
-
 }
